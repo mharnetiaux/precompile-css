aaff2d85f674ce3c902764740fba1800c1c4a792
Matthew Harnetiaux <mattskyh@gmail.com>
2017-08-16 13:44:42 -0700

    Dev (#5)
    * added webpack to compile less and sass.
    
    * removed and changed directories of files.
    
    * added loader for sass.
    
    * updated to babelrc for es6, changed webpack file name for es6. Next up LESS support.

---
+++ webpack.config.babel.js
@@ -0,0 +1,35 @@
+import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import path from 'path';
+
+const EXTRACT_CSS = new ExtractTextPlugin('[name].bundle.css');
+
+
+const config = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+        }, {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+        }, {
+            test: /\.(png|jpg)$/,
+            use: [{
+                loader: 'url-loader',
+                options: { limit: 10000 } // Convert images < 10k to base64 strings
+            }]
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin("bundle.css"),
+    ]
+}
+
+module.exports = config
